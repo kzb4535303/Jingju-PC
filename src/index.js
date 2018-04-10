0e2a39bb5fc571994cb5621bbdf400fff7122c3c
kzb4535303 <562843308@qq.com>
2018-04-10 18:39:30 +0800

    认识框架

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Index from './views/index/Index'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/views/index/Index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Index
